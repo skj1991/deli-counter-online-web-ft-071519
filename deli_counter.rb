991d9cd17f312ef679d9954211f7a22a95473c42
Stacy Johns <stacykjohns@icloud.com>
2019-07-19 14:04:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ def line(array)
      puts "The line is currently empty."
    else
      array.each do |name|
+       num = 0
        customers = "The line is currently: #{num}. #{name}"
-       num += 1
+       name.index + 1
        current_line << customers
    end
      return current_line

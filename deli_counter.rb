266a83894cd877210a3ca3b582a5e26fd2a110a8
Stacy Johns <stacykjohns@icloud.com>
2019-07-19 14:56:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def line(array)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
+  katz_deli.push("name")
   katz_deli.each_with_index do |name, index|
     puts  "Welcome, #{name}. You are number #{index + 1} in line."
   end

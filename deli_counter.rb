9ff856737dfdef6137bb246a3984f430d2b363a0
Stacy Johns <stacykjohns@icloud.com>
2019-07-19 14:13:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,5 +11,18 @@ def line(array)
  end
 end
 
-def take_a_number
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  katz_deli.each_with_index do |n, i|
+    puts  "Welcome, #{n}. You are number #{i + 1} in line."
+  end
+end
+
+def now_serving(deli)
+  if deli.size <= 0
+    puts "There is nobody waiting to be served!"
+  else
+  spot = deli.shift
+  puts "Currently serving #{spot}."
+end
 end

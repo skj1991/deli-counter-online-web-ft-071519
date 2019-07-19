4b8e28352e5971b47d3ca901f27c5c788d09ba04
Stacy Johns <stacykjohns@icloud.com>
2019-07-19 14:02:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,13 @@
-def deli_counter
+def line(array)
+ current_line = []
+   if current_line.size == 0
+     puts "The line is currently empty."
+   else
+     array.each do |name|
+       customers = "The line is currently: #{num}. #{name}"
+       num += 1
+       current_line << customers
+   end
+     return current_line
+ end
 end
